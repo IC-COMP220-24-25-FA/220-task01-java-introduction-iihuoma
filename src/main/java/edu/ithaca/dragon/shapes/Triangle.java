abd6c1aea0202f1b8fb101ec58746841bc249ca8
ifeomaihuoma <iihuoma@ithaca.edu>
2024-09-07 18:10:58 -0400

    Implemented calcArea for Triangle class and passed test

--- src/main/java/edu/ithaca/dragon/shapes/Triangle.java
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -7,13 +7,25 @@ public class Triangle {
     
 
     public Triangle(double base, double height){
+        if (base <= 0){
+            throw new IllegalArgumentException ("base is not positive nunber");
+        }
+
+        if (height <= 0){
+            throw new IllegalArgumentException("height is not positive number");
+        }
+            
+        
         this.base=base;
         this.height=height;
         
     }
 
     public double calcArea(){
-       throw new IllegalArgumentException ("not implemented yet");
+        double areaTriangle = (height * base) / 2;
+        return areaTriangle;
+
+       //throw new IllegalArgumentException ("not implemented yet");
     }
 
     public void doubleSize(){
