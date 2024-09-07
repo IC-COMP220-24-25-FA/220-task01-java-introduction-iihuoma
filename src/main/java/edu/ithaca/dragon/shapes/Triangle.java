1be0eef39d83c2c6ef5f36308ce3c0778552b94c
ifeomaihuoma <iihuoma@ithaca.edu>
2024-09-07 18:01:00 -0400

    Implemented tests for Triangle class

---
+++ src/main/java/edu/ithaca/dragon/shapes/Triangle.java
@@ -0,0 +1,32 @@
+package edu.ithaca.dragon.shapes;
+
+
+public class Triangle {
+    private double base;
+    private double height;
+    
+
+    public Triangle(double base, double height){
+        this.base=base;
+        this.height=height;
+        
+    }
+
+    public double calcArea(){
+       throw new IllegalArgumentException ("not implemented yet");
+    }
+
+    public void doubleSize(){
+        
+    }
+
+    public double longestLineWithin(){
+        throw new IllegalArgumentException ("not implemented yest");
+
+    }
+
+}
+
+    
+
+
